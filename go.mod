201e5f294dca658b45560d26af378f51000d784f
david.choi <david.choi@kakaocorp.com>
2018-09-19 18:45:27 +0900

    initial update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/juhwany/go_plugin
+
+require github.com/juhwany/go_server/v2 v2.0.0
--- plugin.go
+++ plugin.go
@@ -1,6 +1,6 @@
-package main
+package main // import "github.com/juhwany/go_plugin"
 
-import "github.com/juhwany/go_server/apis"
+import "github.com/juhwany/go_server/v2/apis"
 
 func Foo(bar apis.Bar) {
 	bar.DoSomething()
